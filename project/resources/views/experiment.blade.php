7b470e6e7c02ab2aa5a8c9e7f65e9c2dc067e5b2
Raminta-Zurauskaite <ramintules@gmail.com>
2020-05-21 05:44:51 +0300

    Database connection

--- project/resources/views/experiment.blade.php
+++ project/resources/views/experiment.blade.php
@@ -8,7 +8,35 @@
                 <h1 class="text-center">Eksperimento duomenys</h1>
 
                 <div class="card-body">
-                    <h2>Welcome to our Laravel project</h2>
+                    <h2>Įveskite eksperimento duomenis</h2>
+
+                    <form method="post" action="{{ route('cats.store') }}" id="exp_create_form">
+                <div class="form-group">
+                    @csrf
+                    <label for="data"><h3>Data:</h3></label>
+                    <input type="text" class="form-control exp_input" name="data" value="{{old('data')}}"/>
+                </div>
+                <div class="form-group">
+                    <label for="partijoszy"><h3>Bandynių partijos žymuo:</h3></label>
+                    <input type="text" class="form-control exp_input" name="partijoszy" value="{{old('partijoszy')}}"/>
+                </div>
+                <div class="form-group">
+                    <label for="sudetis"><h3>Bandynių sudėtis:</h3></label>
+                    <input type="text" class="form-control exp_input" name="sudetis" value="{{old('sudetis')}}"/>
+                </div>
+                <div class="form-group">
+                    <label for="degimotemp"><h3>Bandynio degimo temperatūra</h3></label>
+                    <input type="text" class="form-control exp_input" name="degimotemp" value="{{old('degimotemp')}}"/>
+                </div>
+                <div class="form-group">
+                    <label for="islaikymot"><h3>Bandinių išlaikymo trukmė aukščiausioje degimo temperatūroje:</h3></label>
+                    <input type="text" class="form-control exp_input" name="islaikymot" value="{{old('islaikymot')}}"/>
+                </div>
+                <button id="submit_cat_create" type="button" class="btn btn-primary"  style="margin-bottom: 20px">
+                    Submit
+                </button>
+
+                    </form>
                 </div>
             </div>
         </div>
