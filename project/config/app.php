cd6c87eb58b39921383b7fffb386c39edf33a688
Raminta-Zurauskaite <ramintules@gmail.com>
2020-05-22 08:42:15 +0300

    Added exporting addons and some formulas

--- project/config/app.php
+++ project/config/app.php
@@ -177,6 +177,7 @@ return [
 
         //Extra added by user
         Collective\Html\HtmlServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
 
@@ -231,6 +232,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
 ];
--- project/resources/views/sample.blade.php
+++ project/resources/views/sample.blade.php
@@ -48,13 +48,14 @@
                 </div>
                 
 
-                <div id = 'msg'>
-                        Atsakymas
-                    </div>
+                
                     <?php
                         echo Form::button('Calculate',['onClick'=>'getMessage()', 'class' => 'btn btn-primary', 'style' => "margin-bottom: 20px"]);
                     ?>
                     <div>
+                    <div id = 'msg'>
+                        Atsakymas
+                    </div>
                     <button type="submit" class="btn btn-primary"  style="margin-bottom: 20px">
                     Siųstis skaičiavimo ataskaitą
                 </button>
@@ -93,6 +94,7 @@
       <script>
          function getMessage() {
                 
+                var r0 = $('input[name=taisyklinga]').val();
                 var r1 = $('input[name=m0]').val();
                 var r2 = $('input[name=m1]').val();
                 var r3 = $('input[name=m2]').val();
@@ -100,7 +102,35 @@
                 var r5 = $('input[name=m4]').val();
                 var r6 = $('input[name=m5]').val();
                 var r7 = $('input[name=m6]').val();
-                var json_kodas = (r1/1) + (r2/1) + (r3/1) + (r4/1);
+                var r8 = $('input[name=hmin]').val();
+                var r9 = $('input[name=hmax]').val();
+                var r10 = $('input[name=aa]').val();
+                var r11 = $('input[name=av]').val();
+                var r12 = $('input[name=ba]').val();
+                var r13 = $('input[name=bv]').val();
+                var r14 = $('input[name=h1]').val();
+                var r15 = $('input[name=h2]').val();
+
+                var S = r10 * r12;
+                if ($(taisyklinga).prop("checked") ){
+                    var V = (((r10/1+r11/1)/2)*((r12/1+r13/1)/2)*((r14/1+r15/1)/2));
+                }
+                else {
+                    var vand_tankis = 1;
+                    var V = (r5-r4)/vand_tankis;
+                }
+
+                var We = (r1/V) * ((r2-r1)/r1) * 100;
+
+                var Wr = (r1/V) * ((r5-r1)/r1) * 100;
+
+                var N = ((r9-r8)/r8);
+
+                var R = (1-(We/Wr))*100;
+
+
+
+                var json_kodas = N;
 
             $.ajaxSetup({
                 headers: {
